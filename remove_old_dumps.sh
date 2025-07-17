0097659ee617c1d3093c39cab656d5322c359e99
maria-zi <102587427+maria-zi@users.noreply.github.com>
2025-07-17 22:33:39 +0400

    Update remove_old_dumps.sh

--- remove_old_dumps.sh
+++ remove_old_dumps.sh
@@ -4,7 +4,8 @@ DUMP_DIR="/tmp/database"
 DATE_THRESHOLD=$(date -d "-14 days" +%Y-%m-%d)
 
 # Ищем файлы с датой в имени и удаляем те, которые старше 14 дней
-find "$DUMP_DIR" -type f -name "*.sql" | while read -r file; do
+find "$DUMP_DIR" -type f -name "*.sql" | 
+while read -r file; do
 
   # Извлекаем дату из имени файла (формат YYYY-MM-DD)
   FILE_DATE=$(basename "$file" | grep -oE "[0-9]{4}-[0-9]{2}-[0-9]{2}")
